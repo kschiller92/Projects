abd316b63da3606fef2a92aea5252f48e614d41c
kschiller92 <kelly.k.schiller@gmail.com>
2015-05-22 10:48:05 -0500

    Create movie.java

---
+++ movie.java
@@ -0,0 +1,15 @@
+//November 5, 2013
+//Kelly Schiller
+//Movie.java
+
+public class Movie {
+    
+        public String title;
+	public String category;
+ 
+	public Movie(String title, String category){
+		this.title = title;
+		this.category = category;
+	}	// end CONSTRUCTOR
+    
+}
